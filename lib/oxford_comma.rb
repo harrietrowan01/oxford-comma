a41f9f1b3e804efea18dfbdaba14a2e4500b935e
harrietrowan01 <harrietrowan01@gmail.com>
2021-03-06 20:58:03 -0500

    commit tree 1465bdd90480d53f28b7d3e2f7ef0c75210c453b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
-def oxford_comma(array)
+require 'pry'
+# def oxford_comma(array)
+    
+#     if array.length==1
+#         return array.join
+#     elsif array.length == 2
+#         return array.join(" and ")
+#     else
+#     array_and = array.insert(-2, "and")
+#          return array_and.join(", ")
 
+#     end
+       
+# end
+def oxford_comma(arr)
+    case arr.size
+    when 0
+        ""
+    when 1
+        arr.first
+    when 2
+        arr.join (" and ")
+    else
+        [arr[0..-2].join(", "), arr.last].join(", and ")
+    end
 end
